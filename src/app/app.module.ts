8a4084b06682161a07c857e798afc09e822ab92f
Gonzalo Corchón <gonzalo.corchon@nurun.com>
2018-10-11 11:20:48 +0200

    Tarea 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LaunchListingComponent } from './components/launch-listing/launch-listing.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AgencyEffects } from './reducers/agency.effects';
+import { MissionEffects } from './reducers/mission.effects';
+import { StatusEffects } from './reducers/status.effects';
+import { LaunchEffects } from './reducers/launch.effects';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    LaunchListingComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AgencyEffects, MissionEffects, StatusEffects, LaunchEffects]),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
